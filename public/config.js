49cf3b7b981619a65931d5e31ed322563d139fe5
sfcdsfcd <silvio-fernando2009@hotmail.com>
2025-06-07 00:34:55 -0300

    correções

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 (() => {
   const isDevelopment = !('update_url' in chrome.runtime.getManifest());
-  window.API_BASE_URL = isDevelopment ? 'http://localhost:3000' : 'https://your-production-url.com';
+  window.API_BASE_URL = isDevelopment ? 'http://localhost:3000/api' : 'https://your-production-url.com';
 })();
--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,6 @@
 const isDevelopment = !('update_url' in chrome.runtime.getManifest());
 const API_BASE_URL = isDevelopment
-  ? 'http://localhost:3000'
+  ? 'http://localhost:3000/api'
   : 'https://your-production-url.com';
 
 
