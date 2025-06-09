68dd1b0e65d3248245443e2476cbe2aa591ead4c
Silvio Fernando <50349062+sfcdsfcd@users.noreply.github.com>
2025-06-09 14:52:02 -0300

    fix: update bootstrap-vue-next plugin usage

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import App from './App.vue'
 import { router } from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import { BootstrapVueNext, BCard, BButton, BFormInput } from 'bootstrap-vue-next'
+import { createBootstrap, BCard, BButton, BFormInput } from 'bootstrap-vue-next'
 
 const app = createApp(App)
 app.use(router)
-app.use(BootstrapVueNext)
+app.use(createBootstrap())
 app.component('BCard', BCard)
 app.component('BButton', BButton)
 app.component('BFormInput', BFormInput)
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
-import { BootstrapVueNext, BCard, BButton, BFormInput } from 'bootstrap-vue-next';
+import { createBootstrap, BCard, BButton, BFormInput } from 'bootstrap-vue-next';
 import { createApp } from 'vue';
 import '../assets/styles/main.scss';
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(BootstrapVueNext);
+app.use(createBootstrap());
 app.component('BCard', BCard);
 app.component('BButton', BButton);
 app.component('BFormInput', BFormInput);
